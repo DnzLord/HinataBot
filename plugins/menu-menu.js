a742755548c83c8ee1ec243e8f0ba3721bad50f5
Dnz <105961719+DnzLord@users.noreply.github.com>
2022-08-16 15:31:13 +0700

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -34,7 +34,7 @@ let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
      }
      let math = max - xp
      let mim_ = ["application/vnd.openxmlformats-officedocument.presentationml.presentation","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet","application/vnd.openxmlformats-officedocument.wordprocessingml.document","text/rtf"]
-     let lin_ = ["https://www.youtube.com","https://www.instagram.com","https://www.facebook.com"]
+     let lin_ = ["https://youtu.be/AbSOnEdByHg","https://www.instagram.com","https://www.facebook.com"]
      
   let cap = `*${htki} Information ${htka}*
 
